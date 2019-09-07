06a3259bd0facf002af210264252c286d88c7173
HP <HP@HOME>
2019-09-07 11:39:34 +0530

    added test

--- mavendemo3/src/test/java/mavendemo3/AzamAdded.java
+++ mavendemo3/src/test/java/mavendemo3/AzamAdded.java
@@ -4,5 +4,14 @@ public class AzamAdded {
 
 public static void main(String[] args) {
 	System.out.println("Amam Added");
+	test();
 }
+	public static void test()
+	{
+		
+		System.out.println("added method test");
+	}
+	
+	
 }
+
